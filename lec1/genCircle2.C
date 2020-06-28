79ef1de5d4a394a7372cc0dfaf849b3e0fde9137
Bo Li <libo0929@gmail.com>
2020-06-28 16:07:50 +0800

    Corrected the histogram.

--- lec1/genCircle2.C
+++ lec1/genCircle2.C
@@ -1,8 +1,6 @@
 void genCircle2()
 {
-	//TH2F* hist = new TH2F("hist", "h", 100, -1, 1, 100, -1, 1);
-	TH2F* hist = new TH2F("hist", "h", 100, 0, 1, 100, 0, 1);
-	//TH2F* hist = new TH2F("hist", "h", 100, 0, 1, 100, 0, 2*TMath::Pi());
+	TH2F* hist = new TH2F("hist", "h", 100, -1, 1, 100, -1, 1);
 
 	gBenchmark->Start("genCircle2");
 
@@ -10,17 +8,14 @@ void genCircle2()
 	{
 		double theta = gRandom->Uniform(0, 2*TMath::Pi());
 		double rho = sqrt( gRandom->Uniform(0, 1) );
-		//double rho = gRandom->Uniform(0, 1);
 
 		double x = rho * cos(theta);
 		double y = rho * sin(theta);
 
 		hist->Fill(x, y);
-		//hist->Fill(theta, rho);
 	}
 
 	gBenchmark->Show("genCircle2");
 
-	//hist->Draw("colz");
-	hist->Draw("colz pol");
+	hist->Draw("colz");
 }
